4699257567bdaca1bfa53ed5504dbe686fcc886a
rkitichotkul <rkitichotkul@gmail.com>
2021-05-02 11:07:19 +0700

    add fjlt implementation

---
+++ fjlt.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def fjlt(A, k):
+    """
+    A variant of FJLT. See the following resources:
+        - The review section (page 3) of https://arxiv.org/abs/1909.04801
+        - Page 1 of https://www.sketchingbigdata.org/fall17/lec/lec9.pdf
+    
+    Note:
+        I name it sfd because the matrices are called S(ample), F(ourier transform), D(iagonal).
+    """
+    d = A.shape[0]
+    sign_vector = np.random.randint(0, 2, size=(d, 1)) * 2 - 1
+    idx = np.zeros(k, dtype=int)
+    idx[1:] = np.random.choice(d - 1, k - 1, replace=False) + 1
+    DA = sign_vector * A
+    FDA = np.fft.fft(DA, axis=0, norm='ortho')
+    A_embedded = np.sqrt(d / k) * FDA[idx]
+    return A_embedded
